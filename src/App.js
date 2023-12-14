10d7e1fa5be86a216d96b37a6c703c198c15a407
Idris Idris <49335945+IdrisAdeyemi01@users.noreply.github.com>
2023-12-14 18:32:14 +0100

    latest push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Lib from './Lib/Lib';
+
+function App() {
+  return (
+    <div className="App">
+      <Lib />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Lib/Lib.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+
+const Lib = () => {
+  const [attires, setAttires] = useState([
+    {
+      id: 1,
+      name: "Culotte dress",
+      likes: 2,
+    },
+
+    {
+      id: 2,
+      name: "Babydoll dress",
+      likes: 4,
+    },
+    {
+      id: 3,
+      name: "Wrap around dress",
+      likes: 0,
+    },
+    {
+      id: 4,
+      name: "Kimono dress",
+      likes: 0,
+    },
+    {
+      id: 5,
+      name: "Bubble dress",
+      likes: 0,
+    },
+    {
+      id: 6,
+      name: "Tutu dress",
+      likes: 0,
+    },
+    {
+      id: 7,
+      name: "Polo dress",
+      likes: 0,
+    },
+    {
+      id: 8,
+      name: "Agbada Dress",
+      likes: 0,
+    },
+    {
+      id: 9,
+      name: "Buba dress",
+      likes: 0,
+    },
+    {
+      id: 10,
+      name: "Gele dress",
+      likes: 0,
+    },
+  ]);
+
+  function handleLikeClick(id) {
+    setAttires((prevAttires) => {
+      return prevAttires.map((attire) =>
+        attire.id === id ? { ...attire, likes: attire.likes + 1 } : attire
+      );
+    });
+  }
+
+  function handleDislikeClick(id) {
+    setAttires((prevAttires) => {
+      return prevAttires.map((attire) =>
+        attire.id === id
+          ? { ...attire, likes: Math.max(0, attire.likes - 1) }
+          : attire
+      );
+    });
+  }
+
+  function handleDeleteClick(id) {
+    setAttires((prevAttires) => {
+      return prevAttires.filter((attire) => attire.id !== id);
+    });
+  }
+
+
+  return (
+    <div className="background">
+      <h1>Fash Vix Marketplace</h1>
+      <div>
+        <h2>Fashion Attires</h2>
+        <div className="attire-list">
+          {attires
+            .sort((a, b) => b.likes - a.likes)
+            .map((attire) => (
+              <div key={attire.id} className="attire-item">
+                <h4>{attire.name}</h4>
+                <p>{attire.likes}</p>
+                <button onClick={() => handleLikeClick(attire.id)}>+</button>
+                <button onClick={() => handleDislikeClick(attire.id)}>-</button>
+                <button onClick={() => handleDeleteClick(attire.id)}>
+                  Delete
+                </button>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Lib;
